0e0e5daf7caa724a0e391aa500f47824736950f0
Nick Sarten <gen.battle@gmail.com>
2015-11-14 22:44:33 +1300

    Eliminated redundant code in the dkm_parallel implementation.
    Also added information about the benchmark data to the README.

--- include/dkm_parallel.hpp
+++ include/dkm_parallel.hpp
@@ -4,6 +4,8 @@
 #ifndef DKM_KMEANS_PARALLEL_H
 #define DKM_KMEANS_PARALLEL_H
 
+#include "dkm.hpp"
+
 #include <vector>
 #include <array>
 #include <tuple>
@@ -24,108 +26,6 @@ These functions are all private implementation details and shouldn't be referenc
 file.
 */
 namespace details {
-
-/*
-Calculate the square of the distance between two points.
-*/
-template <typename T, size_t N>
-T distance_squared(const std::array<T, N>& point_a, const std::array<T, N>& point_b) {
-	T d_squared = T();
-	for (typename std::array<T, N>::size_type i = 0; i < N; ++i) {
-		auto delta = point_a[i] - point_b[i];
-		d_squared += delta * delta;
-	}
-	return d_squared;
-}
-
-/*
-Calculate the smallest distance between each of the data points and any of the input means.
-*/
-template <typename T, size_t N>
-std::vector<T> closest_distance(const std::vector<std::array<T, N>>& means, const std::vector<std::array<T, N>>& data, uint32_t k) {
-	std::vector<T> distances;
-	distances.reserve(k);
-	for (auto&d : data) {	
-		T closest = distance_squared(d, means[0]);
-		for (auto& m: means) {
-			T distance = distance_squared(d, m);
-			if (distance < closest)
-				closest = distance;
-		}
-		distances.push_back(closest);
-	}
-	return distances;
-}
-
-/*
-This is an alternate initialization method based on the [kmeans++](https://en.wikipedia.org/wiki/K-means%2B%2B) 
-initialization algorithm.
-*/
-template <typename T, size_t N>
-std::vector<std::array<T, N>> random_plusplus(const std::vector<std::array<T, N>>& data, uint32_t k) {
-	assert(k > 0);
-	using input_size_t = typename std::array<T,N>::size_type;
-	std::vector<std::array<T, N>> means;
-	// Using a very simple PRBS generator, parameters selected according to 
-	// https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
-	std::random_device rand_device;
-	std::linear_congruential_engine<uint64_t, 6364136223846793005, 1442695040888963407, UINT64_MAX> rand_engine(rand_device());
-	
-	// Select first mean at random from the set
-	{
-		std::uniform_int_distribution<input_size_t> uniform_generator(0, data.size() - 1);
-		means.push_back(data[uniform_generator(rand_engine)]);
-	}
-	
-	for (uint32_t count = 1; count < k; ++count) {
-		// Calculate the distance to the closest mean for each data point
-		auto distances = details::closest_distance(means, data, k);
-		// Pick a random point weighted by the distance from existing means
-		std::discrete_distribution<input_size_t> generator(distances.begin(), distances.end());
-		means.push_back(data[generator(rand_engine)]);
-	}
-	return means;
-}
-
-/*
-Calculate the index of the mean a particular data point is closest to (euclidean distance)
-*/
-template <typename T, size_t N>
-uint32_t closest_mean(const std::array<T, N>& point, const std::vector<std::array<T, N>>& means) {
-	assert(!means.empty());
-	auto calculate_distance = [](const std::array<T, N>& point, const std::array<T, N>& mean){
-		T distance = T();
-		for (size_t i = 0; i < point.size(); ++i) {
-			T difference = point[i] - mean[i];
-			distance += difference * difference;
-		}
-		return distance;
-	};
-	T smallest_distance = calculate_distance(point, means[0]);
-	typename std::array<T, N>::size_type index = 0;
-	T distance;
-	for (size_t i = 1; i < means.size(); ++i) {
-		distance = calculate_distance(point, means[i]);
-		if (distance < smallest_distance) {
-			smallest_distance = distance;
-			index = i;
-		}
-	}
-	return index;
-}
-
-/*
-Calculate the index of the mean each data point is closest to (euclidean distance).
-*/
-template <typename T, size_t N>
-std::vector<uint32_t> calculate_clusters(const std::vector<std::array<T, N>>& data, const std::vector<std::array<T, N>>& means) {
-	std::vector<uint32_t> clusters;
-	clusters.reserve(means.size());
-	for (auto& point : data) {
-		clusters.push_back(closest_mean(point, means));
-	}
-	return clusters;
-}
 
 /*
 Calculate means based on data points and their cluster assignments.
@@ -187,14 +87,14 @@ std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(co
 	static_assert(std::is_arithmetic<T>::value && std::is_signed<T>::value, "kmeans_lloyd requires the template parameter T to be a signed arithmetic type (e.g. float, double, int)");
 	assert(k > 0); // k must be greater than zero
 	assert(data.size() >= k); // there must be at least k data points
-	std::vector<std::array<T, N>> means = details::random_plusplus(data, k);
+	std::vector<std::array<T, N>> means = dkm::details::random_plusplus(data, k);
 	
 	std::vector<std::array<T, N>> old_means;
 	std::vector<uint32_t> clusters;
 	// Calculate new means until convergence is reached
 	int count = 0;
 	do {
-		clusters = details::calculate_clusters(data, means);
+		clusters = dkm::details::calculate_clusters(data, means);
 		old_means = means;
 		means = details::calculate_means(data, clusters, old_means, k);
 		++count;
