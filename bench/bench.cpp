6b4102570d08824bb5afe10a9254a6c08cff3aec
Nick Sarten <gen.battle@gmail.com>
2015-11-15 15:54:38 +1300

    Removed parallel implementation.
    My first attempt at a naive implementation was no faster than the serial
    implementation. In fact, it took generally twice as long. Research
    suggests that this is a difficult algorithm to efficiently parallelize.
    at 400ms for over 2 million data points, there doesn't seem to be a huge
    case for parallelizing parts of the algorithm.

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -5,7 +5,6 @@ This is just simple test harness without any external dependencies.
 */
 
 #include "../include/dkm.hpp"
-#include "../include/dkm_parallel.hpp"
 #include "opencv2/opencv.hpp"
 
 #include <vector>
@@ -47,13 +46,12 @@ cv::Mat load_opencv() {
 	std::ifstream file("iris.data.csv");
 	cv::Mat data;
 	for (auto it = std::istream_iterator<std::string>(file); it != std::istream_iterator<std::string>(); ++it) {
-		auto comma_pos = it->find(",");
-		if (comma_pos != std::string::npos) {
-			cv::Vec<float, 2> values;
-			values[0] = std::stof(it->substr(0, comma_pos));
-			values[1] = std::stof(it->substr(comma_pos + 1));
-			data.push_back(values);
-		}
+		auto split = split_commas(*it);
+		assert(split.size() > 1); // must be at least 2 values per line
+		cv::Vec<float, 2> values;
+		values[0] = std::stof(split[0]);
+		values[1] = std::stof(split[1]);
+		data.push_back(values);
 	}
 	std::cout << "done" << std::endl;
 	return data;
@@ -64,9 +62,9 @@ std::vector<std::array<float, 2>> load_dkm() {
 	std::ifstream file("iris.data.csv");
 	std::vector<std::array<float, 2>> data;
 	for (auto it = std::istream_iterator<std::string>(file); it != std::istream_iterator<std::string>(); ++it) {
-		auto comma_pos = it->find(",");
-		if (comma_pos != std::string::npos)
-			data.push_back({std::stof(it->substr(0, comma_pos)), std::stof(it->substr(comma_pos + 1))});
+		auto split = split_commas(*it);
+		assert(split.size() > 1); // must be at least 2 values per line
+		data.push_back({std::stof(split[0]), std::stof(split[1])});
 	}
 	std::cout << "done" << std::endl;
 	return data;
@@ -116,32 +114,11 @@ std::chrono::duration<double> profile_dkm(std::vector<std::array<float, 2>>& dat
 	return end - start;
 }
 
-std::chrono::duration<double> profile_dkm_parallel(std::vector<std::array<float, 2>>& data, int k) {
-	std::cout << "--- Profiling dkm_parallel kmeans ---" << std::endl;
-	std::cout << "Running kmeans..." << std::endl;
-	auto start = std::chrono::high_resolution_clock::now();
-	auto result = dkm_parallel::kmeans_lloyd(data, k);
-	auto end = std::chrono::high_resolution_clock::now();
-	print_result_dkm(result);
-	std::cout << "done" << std::endl;
-	return end - start;
-}
-
 std::chrono::duration<double> profile_dkm_large(std::vector<std::array<int32_t, 5>>& data, int k) {
 	std::cout << "--- Profiling dkm kmeans with large data set ---" << std::endl;
 	std::cout << "Running kmeans..." << std::endl;
 	auto start = std::chrono::high_resolution_clock::now();
 	auto result = dkm::kmeans_lloyd(data, k);
-	auto end = std::chrono::high_resolution_clock::now();
-	print_result_dkm(result);
-	return end - start;
-}
-
-std::chrono::duration<double> profile_dkm_large_parallel(std::vector<std::array<int32_t, 5>>& data, int k) {
-	std::cout << "--- Profiling dkm_parallel kmeans with large data set ---" << std::endl;
-	std::cout << "Running kmeans..." << std::endl;
-	auto start = std::chrono::high_resolution_clock::now();
-	auto result = dkm_parallel::kmeans_lloyd(data, k);
 	auto end = std::chrono::high_resolution_clock::now();
 	print_result_dkm(result);
 	return end - start;
@@ -153,16 +130,12 @@ int main() {
 	auto time_opencv = profile_opencv(cv_data, 3);
 	auto dkm_data = load_dkm();
 	auto time_dkm = profile_dkm(dkm_data, 3);
-	auto time_dkm_parallel = profile_dkm_parallel(dkm_data, 3);
 	auto dkm_data_large = load_dkm_large();
 	auto time_dkm_large = profile_dkm_large(dkm_data_large, 5);
-	auto time_dkm_large_parallel = profile_dkm_large_parallel(dkm_data_large, 5);
 	
 	std::cout << "OpenCV: " << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_opencv).count() << "ms" << std::endl;
 	std::cout << "DKM: " << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_dkm).count() << "ms" << std::endl;
 	std::cout << "DKM Large: " << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_dkm_large).count() << "ms" << std::endl;
-	std::cout << "DKM Parallel: " << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_dkm_parallel).count() << "ms" << std::endl;
-	std::cout << "DKM Parallel Large: " << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_dkm_large_parallel).count() << "ms" << std::endl;
 	
 	return 0;
 }
