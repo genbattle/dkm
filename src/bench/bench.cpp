3cf50572793a78e142213220bcf1376954d02956
Nick Sarten <gen.battle@gmail.com>
2015-12-22 18:56:59 +1300

    Removed large test to comply with Github's size policies.

--- src/bench/bench.cpp
+++ src/bench/bench.cpp
@@ -70,28 +70,6 @@ std::vector<std::array<float, 2>> load_dkm() {
 	return data;
 }
 
-std::vector<std::array<int32_t, 5>> load_dkm_large() {
-	std::cout << "Loading large dkm dataset..." << std::endl;
-	std::ifstream file("USCensus1990.data.csv");
-	std::vector<std::array<int32_t, 5>> data;
-	// extract columns 1, 15, 16, 38, 56
-	uint64_t count = 0;
-	for (auto it = std::istream_iterator<std::string>(file); it != std::istream_iterator<std::string>(); ++it) {
-		auto split = split_commas(*it);
-		assert(split.size() > 56);
-		data.push_back({std::stoi(split[1]),
-			std::stoi(split[15]),
-			std::stoi(split[16]),
-			std::stoi(split[38]),
-			std::stoi(split[56])});
-		++count;
-		if (count % 100000 == 0)
-			std::cout << "...parsed " << count << " records" << std::endl;
-	}
-	std::cout << "done" << std::endl;
-	return data;
-}
-
 std::chrono::duration<double> profile_opencv(const cv::Mat& data, int k) {
 	std::cout << "--- Profiling OpenCV kmeans ---" << std::endl;
 	std::cout << "Running kmeans..." << std::endl;
@@ -103,10 +81,7 @@ std::chrono::duration<double> profile_opencv(const cv::Mat& data, int k) {
 			data, k, labels, cv::TermCriteria(cv::TermCriteria::EPS, 0, 0.01), 1, cv::KMEANS_PP_CENTERS, centers);
 		(void)labels;
 		std::cout << "centers: ";
-		for (auto it = centers.begin<cv::Vec<float, 2>>(); it != centers.end<cv::Vec<float, 2>>(); ++it) {
-			std::cout << "(" << (*it)[0] << "," << (*it)[1] << "), ";
-		}
-		std::cout << "\n";
+		std::cout << centers << std::endl;
 	}
 	auto end = std::chrono::high_resolution_clock::now();
 	std::cout << std::endl;
@@ -127,33 +102,18 @@ std::chrono::duration<double> profile_dkm(const std::vector<std::array<float, 2>
 	return (end - start) / 10.0;
 }
 
-std::chrono::duration<double> profile_dkm_large(const std::vector<std::array<int32_t, 5>>& data, int k) {
-	std::cout << "--- Profiling dkm kmeans with large data set ---" << std::endl;
-	std::cout << "Running kmeans..." << std::endl;
-	auto start = std::chrono::high_resolution_clock::now();
-	auto result = dkm::kmeans_lloyd(data, k);
-	auto end = std::chrono::high_resolution_clock::now();
-	print_result_dkm(result);
-	return end - start;
-}
-
 int main() {
 	std::cout << "# BEGINNING PROFILING #\n" << std::endl;
 	auto cv_data = load_opencv();
 	auto time_opencv = profile_opencv(cv_data, 3);
 	auto dkm_data = load_dkm();
 	auto time_dkm = profile_dkm(dkm_data, 3);
-	auto dkm_data_large = load_dkm_large();
-	auto time_dkm_large = profile_dkm_large(dkm_data_large, 5);
 
 	std::cout << "OpenCV: "
 			  << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_opencv).count() << "ms"
 			  << std::endl;
 	std::cout << "DKM: " << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_dkm).count()
 			  << "ms" << std::endl;
-	std::cout << "DKM Large: "
-			  << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(time_dkm_large).count() << "ms"
-			  << std::endl;
 
 	return 0;
 }
