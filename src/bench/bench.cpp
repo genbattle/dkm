c78d692b76bbdd1e7a71d4e1b2b93fdf85f5d8ba
Nick Sarten <gen.battle@gmail.com>
2018-10-24 23:07:21 +1300

    Made benchmark data loading functions generic.
    Both loading functions can now load any data type and dimensionality.
    This will come in handy for the new test files I plan on adding. The
    disadvantage to the current implementation is that the data is initially
    parsed as a double precision floating point value before being converted
    to the final type, so some inaccuracy may be introduced for large 64-bit
    integers (which none of my data sets should contain).

--- src/bench/bench.cpp
+++ src/bench/bench.cpp
@@ -42,34 +42,42 @@ void print_result_dkm(std::tuple<std::vector<std::array<T, N>>, std::vector<uint
 	std::cout << std::endl;
 }
 
-cv::Mat load_opencv() {
-	std::cout << "Loading small OpenCV dataset...";
-	std::ifstream file("iris.data.csv");
+template <typename T, size_t N>
+cv::Mat load_opencv(const std::string& path) {
+	std::cout << "Loading OpenCV dataset " << path << "..." << std::flush;
+	std::ifstream file(path);
 	cv::Mat data;
 	for (auto it = std::istream_iterator<std::string>(file); it != std::istream_iterator<std::string>(); ++it) {
 		auto split = split_commas(*it);
-		assert(split.size() > 1); // must be at least 2 values per line
-		cv::Vec<float, 2> values;
-		values[0] = std::stof(split[0]);
-		values[1] = std::stof(split[1]);
+		assert(split.size() == N); // number of values in file must match expected row size
+		cv::Vec<T, N> values;
+		for (size_t i = 0; i < N; ++i) {
+			values[i] = static_cast<T>(std::stod(split[i]));
+		}
 		data.push_back(values);
 	}
 	std::cout << "done" << std::endl;
 	return data;
 }
 
-std::vector<std::array<float, 2>> load_dkm() {
-	std::cout << "Loading small dkm dataset...";
-	std::ifstream file("iris.data.csv");
-	std::vector<std::array<float, 2>> data;
+template <typename T, size_t N>
+std::vector<std::array<T, N>> load_dkm(const std::string& path) {
+	std::cout << "Loading dkm dataset" << path << "..." << std::flush;
+	std::ifstream file(path);
+	std::vector<std::array<float, N>> data;
 	for (auto it = std::istream_iterator<std::string>(file); it != std::istream_iterator<std::string>(); ++it) {
 		auto split = split_commas(*it);
-		assert(split.size() > 1); // must be at least 2 values per line
-		data.push_back({{std::stof(split[0]), std::stof(split[1])}});
+		assert(split.size() == N); // number of values must match rows in file
+		std::array<T, N> row;
+		std::transform(split.begin(), split.end(), row.begin(), [](const std::string& in) -> T {
+			return static_cast<T>(std::stod(in));
+		});
+		data.push_back(row);
 	}
 	std::cout << "done" << std::endl;
 	return data;
 }
+
 
 std::chrono::duration<double> profile_opencv(const cv::Mat& data, int k) {
 	std::cout << "--- Profiling OpenCV kmeans ---" << std::endl;
@@ -118,9 +126,9 @@ std::chrono::duration<double> profile_dkm_parallel(const std::vector<std::array<
 
 int main() {
 	std::cout << "# BEGINNING PROFILING #\n" << std::endl;
-	auto cv_data = load_opencv();
+	auto cv_data = load_opencv<float, 2>("iris.data.csv");
 	auto time_opencv = profile_opencv(cv_data, 3);
-	auto dkm_data = load_dkm();
+	auto dkm_data = load_dkm<float, 2>("iris.data.csv");
 	auto time_dkm = profile_dkm(dkm_data, 3);
 	auto time_dkm_parallel = profile_dkm_parallel(dkm_data, 3);
 
