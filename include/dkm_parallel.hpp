cd00f6a174d0df364fc54dbf82883cf7aaf0b711
Nick Sarten <gen.battle@gmail.com>
2019-08-15 21:17:35 +1200

    Fixed bug in parallel implementation.
    Also modified travis build to build release version rather than debug.

--- include/dkm_parallel.hpp
+++ include/dkm_parallel.hpp
@@ -141,7 +141,7 @@ std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd_pa
 		++count;
 	} while ((means != old_means && means != old_old_means)
 		|| (parameters.has_max_iteration() && count == parameters.get_max_iteration())
-		|| (parameters.has_min_delta() && !details::deltas_below_limit(details::deltas(old_means, means), parameters.get_min_delta())));
+		|| (parameters.has_min_delta() && details::deltas_below_limit(details::deltas(old_means, means), parameters.get_min_delta())));
 
 	return std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>>(means, clusters);
 }
